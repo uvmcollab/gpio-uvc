34d5870a3653b5bce6492ffac74732fc89b25299
Nami-L <namigtle066@gmail.com>
2025-10-13 14:49:01 -0600

    feat: add transaction logic

--- sv/gpio_uvc_types.sv
+++ sv/gpio_uvc_types.sv
@@ -1,4 +1,17 @@
 `ifndef GPIO_UVC_TYPES_SV
 `define GPIO_UVC_TYPES_SV
 
-`endif // GPIO_UVC_TYPES_SV
+parameter int unsigned GPIO_UVC_MAX_WIDTH = 32;
+typedef bit [GPIO_UVC_MAX_WIDTH-1:0] gpio_uvc_data_t;
+
+typedef enum {
+  GPIO_UVC_ITEM_SYNC,
+  GPIO_UVC_ITEM_ASYNC
+} gpio_uvc_item_type_e;
+
+typedef enum {
+  GPIO_UVC_ITEM_DELAY_OFF,
+  GPIO_UVC_ITEM_DELAY_ON
+} gpio_uvc_item_delay_e;
+
+`endif  // GPIO_UVC_TYPES_SV
