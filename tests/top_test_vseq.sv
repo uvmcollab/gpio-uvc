3a6e3caeb2c68cd2f193c8182a4fd11da4edc73b
Nami-L <namigtle066@gmail.com>
2025-11-19 17:55:40 -0600

    feat: test using gpio_uvc_file

--- tests/top_test_vseq.sv
+++ tests/top_test_vseq.sv
@@ -10,10 +10,10 @@ class top_test_vseq extends uvm_sequence;
 
   extern function new(string name = "");
 
-  extern task gpio_uvc_seq();
+//  extern task gpio_uvc_seq();
 //  extern task gpio_uvc_rst();
   extern task gpio_uvc_pulse_rst();
-//  extern task gpio_uvc_file(string filename);
+  extern task gpio_uvc_file(string filename);
   extern task body();
   constraint iter_c {iter inside {[1 : 100]};}
 endclass : top_test_vseq
@@ -26,30 +26,30 @@ function top_test_vseq::new(string name = "");
 endfunction : new
 
 
- task top_test_vseq::gpio_uvc_seq();
-   // Write your sequence here
-   gpio_uvc_sequence_base seq;
-   seq = gpio_uvc_sequence_base::type_id::create("seq");
-   if (!seq.randomize() with {
-
-         m_trans.m_gpio_pin inside {[10 : 20]};
-         m_trans.m_trans_type == GPIO_UVC_ITEM_SYNC;
-         m_trans.m_delay_enable == GPIO_UVC_ITEM_DELAY_ON;
-       //  m_trans.m_align_type == GPIO_UVC_ITEM_ALIGN_TYPE_RISING;
-       }) begin
-     `uvm_fatal(get_name(), "Failed to randomize sequence")
-   end
-   // Mando las senceucnias que acabo de randomizar haceia el sequencer
-   // Ejecuta esta secuencia (seq) que configure(seqeunce_base _pulse)
-   // sobre este secuenciador (m_gpio_data_sequencer) que pertenece a p_sequencer
-   //UVM entra automaticamente al body
-   seq.start(p_sequencer.m_gpio_data_sequencer);
-
-  //p_sequencer es mi virtual sequence(top_vsqr), es un grupo de secuenciadores que tiene varios agentes
-// “Del grupo p_sequencer, usa el secuenciador m_gpio_data_sequencer
-//y ejecuta allí mi secuencia seq. Luego deja que UVM llame al body()
-//para generar y mandar la transacción al DUT.”
- endtask : gpio_uvc_seq
+// task top_test_vseq::gpio_uvc_seq();
+//   // Write your sequence here
+//   gpio_uvc_sequence_base seq;
+//   seq = gpio_uvc_sequence_base::type_id::create("seq");
+//   if (!seq.randomize() with {
+//
+//         m_trans.m_gpio_pin inside {[10 : 20]};
+//         m_trans.m_trans_type == GPIO_UVC_ITEM_SYNC;
+//         m_trans.m_delay_enable == GPIO_UVC_ITEM_DELAY_ON;
+//       //  m_trans.m_align_type == GPIO_UVC_ITEM_ALIGN_TYPE_RISING;
+//       }) begin
+//     `uvm_fatal(get_name(), "Failed to randomize sequence")
+//   end
+//   // Mando las senceucnias que acabo de randomizar haceia el sequencer
+//   // Ejecuta esta secuencia (seq) que configure(seqeunce_base _pulse)
+//   // sobre este secuenciador (m_gpio_data_sequencer) que pertenece a p_sequencer
+//   //UVM entra automaticamente al body
+//   seq.start(p_sequencer.m_gpio_data_sequencer);
+//
+//  //p_sequencer es mi virtual sequence(top_vsqr), es un grupo de secuenciadores que tiene varios agentes
+//// “Del grupo p_sequencer, usa el secuenciador m_gpio_data_sequencer
+////y ejecuta allí mi secuencia seq. Luego deja que UVM llame al body()
+////para generar y mandar la transacción al DUT.”
+// endtask : gpio_uvc_seq
 
 // task top_test_vseq::gpio_uvc_rst();
 //   // Write your sequence here
@@ -84,22 +84,25 @@ seq = gpio_uvc_sequence_pulse::type_id::create("seq");
 if (! seq.randomize() with { 
     m_pin_assert.m_gpio_pin == 1'b1;
     m_pin_assert.m_trans_type == GPIO_UVC_ITEM_SYNC;
-    m_pin_assert.m_delay_enable == GPIO_UVC_ITEM_DELAY_OFF;    
+    m_pin_assert.m_delay_enable == GPIO_UVC_ITEM_DELAY_OFF;
+    m_pin_assert.m_align_type == GPIO_UVC_ITEM_ALIGN_TYPE_RISING;    
     m_pin_deassert.m_gpio_pin == 1'b0;
     m_pin_deassert.m_trans_type == GPIO_UVC_ITEM_SYNC;
-    m_pin_deassert.m_delay_enable == GPIO_UVC_ITEM_DELAY_OFF;   
+    m_pin_deassert.m_delay_enable == GPIO_UVC_ITEM_DELAY_OFF;  
+    m_pin_deassert.m_align_type == GPIO_UVC_ITEM_ALIGN_TYPE_RISING;    
+ 
          }) begin
      `uvm_fatal(get_name(), "Failed to randomize sequence")
    end
   seq.start(p_sequencer.m_gpio_rst_sequencer);
 endtask: gpio_uvc_pulse_rst
 
-//  task top_test_vseq::gpio_uvc_file(string filename);
-//  gpio_uvc_sequence_from_file seq;
-//  seq = gpio_uvc_sequence_from_file::type_id::create("seq");
-//  seq.m_file_name = {`GIT_DIR, filename};
-//  seq.start(p_sequencer.m_gpio_data_sequencer);
-//  endtask: gpio_uvc_file
+  task top_test_vseq::gpio_uvc_file(string filename);
+  gpio_uvc_sequence_from_file seq;
+  seq = gpio_uvc_sequence_from_file::type_id::create("seq");
+  seq.m_file_name = {`GIT_DIR, filename};
+  seq.start(p_sequencer.m_gpio_data_sequencer);
+  endtask: gpio_uvc_file
 
 
 task top_test_vseq::body();
@@ -108,9 +111,9 @@ task top_test_vseq::body();
   // Initial delay
   #(200ns);
 
-  repeat (iter) begin
-    gpio_uvc_seq();
-    //gpio_uvc_file("/sv/seqlib/sample.seq");
+  repeat (1) begin
+    //gpio_uvc_seq();
+    gpio_uvc_file("/sv/seqlib/sample.seq");
   end
 
   // Drain time 
