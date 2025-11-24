198874650f8fe183e42235f556c3080391aff84a
Nami-L <namigtle066@gmail.com>
2025-11-24 17:13:49 -0600

    feat: add config and agent into env

--- env/top_env.sv
+++ env/top_env.sv
@@ -7,17 +7,16 @@ class top_env extends uvm_env;
 
   top_env_config    m_config;
 
-  gpio_uvc_config   m_gpio_uvc_data_config;
-  gpio_uvc_agent    m_gpio_uvc_data_agent;
+  gpio_uvc_config   m_port_rst_config;
+  gpio_uvc_agent    m_port_rst_agent;
   
-  gpio_uvc_config   m_gpio_uvc_rst_config;
-  gpio_uvc_agent    m_gpio_uvc_rst_agent;
+
 
   top_vsqr          vsqr;
 
-  top_scoreboard    m_scoreboard;
+  //top_scoreboard    m_scoreboard;
 
-  top_coverage      m_coverage;
+  //top_coverage      m_coverage;
 
   extern function new(string name, uvm_component parent);
 
@@ -37,37 +36,24 @@ function void top_env::build_phase(uvm_phase phase);
     `uvm_fatal(get_name(), "Could not retrieve top_env_config from config db")
   end
 
-  // ========================== GPIO_UVC DATA AGENT =========================== //
-  m_gpio_uvc_data_config = gpio_uvc_config::type_id::create("m_gpio_uvc_data_config");
-  m_gpio_uvc_data_config.is_active = UVM_ACTIVE;
-  m_gpio_uvc_data_config.gpio_width = 'd32;
-  m_gpio_uvc_data_config.start_value = 'd7;
+  // ========================== PORT RESET AGENT ========================== //
+  m_port_rst_config = gpio_uvc_config::type_id::create("m_port_rst_config");
+  m_port_rst_config.is_active = UVM_ACTIVE;
+  m_port_rst_config.gpio_width = 'd1;
+  m_port_rst_config.start_value = 'd0;
 
-  if (!uvm_config_db#(virtual gpio_uvc_if)::get(this, "m_gpio_uvc_data_agent", "vif", m_gpio_uvc_data_config.vif)) begin
-    `uvm_fatal(get_name(), "Could not retrieve m_gpio_uvc_data_config from config db")
+  if (!uvm_config_db#(virtual gpio_uvc_if)::get(this, "m_port_rst_agent", "vif", m_port_rst_config.vif)) begin
+    `uvm_fatal(get_name(), "Could not retrieve m_port_rst_config from config db")
   end
-  
-  uvm_config_db #(gpio_uvc_config)::set(this, "m_gpio_uvc_data_agent", "config", m_gpio_uvc_data_config);
-  m_gpio_uvc_data_agent = gpio_uvc_agent::type_id::create("m_gpio_uvc_data_agent", this);
 
-  // ========================== GPIO_UVC RESET AGENT ========================== //
-  m_gpio_uvc_rst_config = gpio_uvc_config::type_id::create("m_gpio_uvc_rst_config");
-  m_gpio_uvc_rst_config.is_active = UVM_ACTIVE;
-  m_gpio_uvc_rst_config.gpio_width = 'd1;
-  m_gpio_uvc_rst_config.start_value = 'd0;
-
-  if (!uvm_config_db#(virtual gpio_uvc_if)::get(this, "m_gpio_uvc_rst_agent", "vif", m_gpio_uvc_rst_config.vif)) begin
-    `uvm_fatal(get_name(), "Could not retrieve m_gpio_uvc_rst_config from config db")
-  end
-  
-  uvm_config_db #(gpio_uvc_config)::set(this, "m_gpio_uvc_rst_agent", "config", m_gpio_uvc_rst_config);
-  m_gpio_uvc_rst_agent = gpio_uvc_agent::type_id::create("m_gpio_uvc_rst_agent", this);
+  uvm_config_db #(gpio_uvc_config)::set(this, "m_port_rst_agent", "config", m_port_rst_config);
+  m_port_rst_agent = gpio_uvc_agent::type_id::create("m_port_rst_agent", this);
 
   // ================================= COVERAGE =============================== //
-  m_coverage  = top_coverage::type_id::create("m_coverage", this);
+  //m_coverage  = top_coverage::type_id::create("m_coverage", this);
 
   // =============================== SCOREBOARD =============================== //
-  m_scoreboard = top_scoreboard::type_id::create("m_scoreboard", this);
+  //m_scoreboard = top_scoreboard::type_id::create("m_scoreboard", this);
 
   // =========================== VIRTUAL SEQUENCER ============================ //
   vsqr = top_vsqr::type_id::create("vsqr", this);
@@ -77,15 +63,15 @@ endfunction : build_phase
 
 function void top_env::connect_phase(uvm_phase phase);
   // ===================== VIRTUAL SEQUENCER CONNECTIONS ====================== //
-  vsqr.m_gpio_rst_sequencer  = m_gpio_uvc_rst_agent.m_sequencer;
-  vsqr.m_gpio_data_sequencer = m_gpio_uvc_data_agent.m_sequencer;
+  vsqr.m_port_rst_sequencer  = m_port_rst_agent.m_sequencer;
+
 
   // ========================= SCOREBOARD CONNECTIONS ========================= //
-  m_gpio_uvc_data_agent.analysis_port.connect(m_scoreboard.gpio_data_imp_export);
+  //m_gpio_uvc_data_agent.analysis_port.connect(m_scoreboard.gpio_data_imp_export);
   //m_gpio_rst_sequencer.analysis_port.connect(m_scoreboard.gpio_rst_imp_export);
 
   // =========================== COVERAGE CONNECTIONS ========================= //
-  m_gpio_uvc_data_agent.analysis_port.connect(m_coverage.gpio_data_imp_export);
+  //(m_gpio_uvc_data_agent.analysis_port.connect(m_coverage.gpio_data_imp_export);
 
 endfunction : connect_phase
 
