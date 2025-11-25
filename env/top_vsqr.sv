b31e09387879ebdabd859c164abb416c8354c11a
Nami-L <namigtle066@gmail.com>
2025-11-24 22:38:05 -0600

    feat: add port c for sequencer

--- env/top_vsqr.sv
+++ env/top_vsqr.sv
@@ -8,6 +8,7 @@ class top_vsqr extends uvm_sequencer;
   gpio_uvc_sequencer m_port_rst_sequencer;
   gpio_uvc_sequencer m_port_a_sequencer;
   gpio_uvc_sequencer m_port_b_sequencer;
+  gpio_uvc_sequencer m_port_c_sequencer;
 
   extern function new(string name, uvm_component parent);
 
