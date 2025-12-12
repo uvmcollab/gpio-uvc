b37c99773e969ec8e8bbd27c8452f49ba2fefc16
Ciro Bermudez <51069488+cirofabianbermudez@users.noreply.github.com>
2025-12-12 10:55:35 +0100

    refactor: remove GPIO_UVC_ROOT prefix from file paths

--- sve.f
+++ sve.f
@@ -1,7 +1,7 @@
-+incdir+$GPIO_UVC_ROOT/rtl
-+incdir+$GPIO_UVC_ROOT/env
-+incdir+$GPIO_UVC_ROOT/tests
-+incdir+$GPIO_UVC_ROOT/tb
-$GPIO_UVC_ROOT/env/top_env_pkg.sv
-$GPIO_UVC_ROOT/tests/top_test_pkg.sv
-$GPIO_UVC_ROOT/tb/tb.sv
++incdir+rtl
++incdir+env
++incdir+tests
++incdir+tb
+env/top_env_pkg.sv
+tests/top_test_pkg.sv
+tb/tb.sv
